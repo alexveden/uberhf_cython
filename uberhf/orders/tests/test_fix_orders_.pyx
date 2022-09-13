e57357bff8fbb5483a5c6afe51dd54ba47816270
Alex Veden <i@alexveden.com>
2022-09-13 16:24:47 +0400

    - fix_orders.pyx - implemented rejects

--- uberhf/orders/tests/test_fix_orders_.pyx
+++ uberhf/orders/tests/test_fix_orders_.pyx
@@ -473,7 +473,7 @@ class CyFIXOrdersTestCase(unittest.TestCase):
 
     def test_exec_sequence__vanilla_fill(self):
         """
-
+        A.1.a – Filled order
         https://www.fixtrading.org/online-specification/order-state-changes/#a-vanilla-1
 
         :return:
@@ -547,4 +547,88 @@ class CyFIXOrdersTestCase(unittest.TestCase):
         assert o.qty == 10
         assert o.cum_qty == 10
         assert o.leaves_qty == 0
+
+
+    def test_exec_sequence__vanilla_fill_reject__pendingnew(self):
+        """
+        A.1.a – Filled ordern (reject)
+        https://www.fixtrading.org/online-specification/order-state-changes/#a-vanilla-1
+
+        :return:
+        """
+        o = FIXNewOrderSingle.create(&q, 1010, 200, qty=10)
+        assert o.status == FIX_OS_CREA, f'o.status={chr(o.status)}'
+
+        ft = FIXTester()
+        assert ft.order_register_single(o) == 1
+        assert o.status == FIX_OS_CREA, f'o.status={chr(o.status)}'
+
+        cdef FIXMsgC msg = ft.fix_exec_report_msg(o,
+                                                  o.clord_id,
+                                                  FIX_ET_PNEW,
+                                                  FIX_OS_PNEW)
+        assert o.process_execution_report(msg.m) == 1
+        assert o.status == FIX_OS_PNEW, f'o.status={chr(o.status)}'
+
+        msg = ft.fix_exec_report_msg(o,
+                                     o.clord_id,
+                                     FIX_ET_REJ,
+                                     FIX_OS_REJ,
+                                     cum_qty=0,
+                                     leaves_qty=0
+                                     )
+        assert o.process_execution_report(msg.m) == 1
+        assert o.status == FIX_OS_REJ, f'o.status={chr(o.status)}'
+        assert o.qty == 10
+        assert o.cum_qty == 0
+        assert o.leaves_qty == 00
+
+    def test_exec_sequence__vanilla_fill__reject_new(self):
+        """
+        A.1.a – Filled order
+        https://www.fixtrading.org/online-specification/order-state-changes/#a-vanilla-1
+
+        :return:
+        """
+        o = FIXNewOrderSingle.create(&q, 1010, 200, qty=10)
+        assert o.status == FIX_OS_CREA, f'o.status={chr(o.status)}'
+
+        ft = FIXTester()
+        assert ft.order_register_single(o) == 1
+        assert o.status == FIX_OS_CREA, f'o.status={chr(o.status)}'
+
+        cdef FIXMsgC msg = ft.fix_exec_report_msg(o,
+                                                  o.clord_id,
+                                                  FIX_ET_PNEW,
+                                                  FIX_OS_PNEW)
+        assert o.process_execution_report(msg.m) == 1
+        assert o.status == FIX_OS_PNEW, f'o.status={chr(o.status)}'
+
+        msg = ft.fix_exec_report_msg(o,
+                                     o.clord_id,
+                                     FIX_ET_NEW,
+                                     FIX_OS_NEW,
+                                     cum_qty=0,
+                                     leaves_qty=10
+                                     )
+        assert o.process_execution_report(msg.m) == 1
+        assert o.status == FIX_OS_NEW, f'o.status={chr(o.status)}'
+        assert o.qty == 10
+        assert o.cum_qty == 0
+        assert o.leaves_qty == 10
+
+        msg = ft.fix_exec_report_msg(o,
+                                     o.clord_id,
+                                     FIX_ET_REJ,
+                                     FIX_OS_REJ,
+                                     cum_qty=0,
+                                     leaves_qty=0
+                                     )
+        assert o.process_execution_report(msg.m) == 1
+        assert o.status == FIX_OS_REJ, f'o.status={chr(o.status)}'
+        assert o.qty == 10
+        assert o.cum_qty == 0
+        assert o.leaves_qty == 00
+
+
 
